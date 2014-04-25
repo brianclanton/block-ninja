a533f8b4c182daa9529113b92478044f479b448c
unknown <bdc1710@SP2530-4.main.ad.rit.edu>
2014-04-25 02:56:50 -0400

    Started work on creating a dialogue box for cutscenes with storyboard elements.

---
+++ MinimalBlockNinja/Assets/Scripts/Dialogue.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dialogue : MonoBehaviour {
+
+	public string[] dialogueElements;
+	public GameObject[] backgrounds;
+
+	private int currentIndex = 0;
+	private string currentText = "Asdf";
+
+	// Use this for initialization
+	void Start () {
+		currentText = dialogueElements[currentIndex++];
+		Debug.Log (currentText);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!IsDone() && Input.GetButtonDown("Advance Dialogue")) {
+			currentText = dialogueElements[currentIndex++];
+			
+			Debug.Log (currentText);
+		}
+	}
+
+	void OnGUI() {
+		// Style stuff
+		GUI.skin.box.alignment = TextAnchor.UpperLeft;
+		GUI.skin.box.padding = new RectOffset(25, 25, 25, 25);
+		GUI.skin.box.fontSize = 24;
+		GUI.skin.box.wordWrap = true;
+
+		// Actual dialog box
+		GUI.Box(new Rect(475, Screen.height - 150, Screen.width - 950, 150), currentText);
+		GUI.Label(new Rect(Screen.width - 475 - 175, Screen.height - 25, 200, 30), "Press Space to continue...");
+	}
+
+	public bool IsDone() {
+		return currentIndex == dialogueElements.Length;
+	}
+}
