7fcead1d0a020bec8dd38cbd9069b21f8d6781d9
Brian Clanton <brian.clanton@gmail.com>
2014-04-30 12:41:06 -0400

    Stopped tracking more userprefs and slns. Added a music manager to play background music.

---
+++ Minimal/Assets/Scripts/MusicManagerSingleton.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManagerSingleton : MonoBehaviour {
+
+	public static MusicManagerSingleton instance = null;
+
+	public static MusicManagerSingleton Instance {
+		get { return instance; }
+	}
+
+	private static float currentTime;
+
+	void Awake() {
+		if (instance != null && instance != this) {
+			Destroy(this.gameObject);
+			return;
+		} else {
+			instance = this;
+		}
+
+		DontDestroyOnLoad(this.gameObject);
+	}
+
+	void Update() {
+		currentTime = audio.time;
+	}
+
+	void OnLevelWasLoaded(int level) {
+		audio.time = currentTime;
+	}
+}
